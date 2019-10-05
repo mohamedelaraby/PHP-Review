d9aa2b32d011f4c631d80eb8e76cdc10e992f196
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-05 16:32:19 +0200

    GET SuperGlobal review

--- website-two/index.php
+++ website-two/index.php
@@ -1,42 +1,14 @@
 
+
+
 <?php include 'server-info.php'; ?>
+<?php include './include/header.php'; ?>
+
+<!------------[HOME page content ]-------------->
+<?php include './include/form.php'; ?>
 
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title> System Server Ingredients - A client $ Server sides information about current system</title>
-    <link rel="styesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+<!------------[last Home page content ]-------------->
 
-  </head>
-  <body>
-    <div class="container">
-      <h1>Server & File Info</h1>
-      <?php if($server): ?>
-        <ul class="list-group">
-          <?php foreach ($server as $info => $value): ?>
-            <li class="list-group-item">
-              <strong><?php echo $info; ?> :</strong>
-              <?php echo $value;  ?>
-            </li>
-          <?php endforeach; ?>
-        </ul>
-      <?php endif;?>
+<?php include './include/footer.php'; ?>
 
-
-      <!---[Client side info]--->
-      <h1>Client & File Info</h1>
-      <?php if($server) : ?>
-        <ul class="list-group">
-            <?php foreach($client as $info => $value) :?>
-            <li class="list-group-item">
-              <Strong><em><?php echo $info; ?> :</em></Strong>
-                <?php echo $value; ?>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-      <?php  endif;?>
-    </div>
-  </body>
-  </html>
