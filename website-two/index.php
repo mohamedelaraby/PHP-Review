c0ef4efd2d668ce6b3dc78219427a8150c5cc660
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-05 11:23:41 +0200

    display full server information

--- website-two/index.php
+++ website-two/index.php
@@ -7,14 +7,36 @@
   <head>
     <meta charset="utf-8">
     <title> System Server Ingredients - A client $ Server sides information about current system</title>
-    <link rel="styesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="styesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
 
   </head>
   <body>
     <div class="container">
       <h1>Server & File Info</h1>
+      <?php if($server): ?>
+        <ul class="list-group">
+          <?php foreach ($server as $info => $value): ?>
+            <li class="list-group-item">
+              <strong><?php echo $info; ?> :</strong>
+              <?php echo $value;  ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif;?>
+
+
+      <!---[Client side info]--->
+      <h1>Client & File Info</h1>
+      <?php if($server) : ?>
+        <ul class="list-group">
+            <?php foreach($client as $info => $value) :?>
+            <li class="list-group-item">
+              <Strong><em><?php echo $info; ?> :</em></Strong>
+                <?php echo $value; ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+      <?php  endif;?>
     </div>
   </body>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
   </html>
